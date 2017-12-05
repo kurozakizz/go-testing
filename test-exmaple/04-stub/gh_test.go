313bc0a022af0a17931c03c24b37b254eb35bcaf
kurozakizz <squallcope@gmail.com>
2017-12-05 19:46:27 +0700

    Change struct prefix from stub to fake

--- test-exmaple/04-stub/gh_test.go
+++ test-exmaple/04-stub/gh_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 )
 
-type StubReleaseInfo struct {
+type FakeReleaseInfo struct {
 	Tag string
 	Err error
 }
 
-func (r StubReleaseInfo) GetLatestReleaseTag(repo string) (string, error) {
+func (r FakeReleaseInfo) GetLatestReleaseTag(repo string) (string, error) {
 	if r.Err != nil {
 		return "", r.Err
 	}
@@ -18,7 +18,7 @@ func (r StubReleaseInfo) GetLatestReleaseTag(repo string) (string, error) {
 }
 
 func TestGetReleaseTagMessage_V010_ShouldReturn_V010(t *testing.T) {
-	releaseInfo := StubReleaseInfo{
+	releaseInfo := FakeReleaseInfo{
 		Tag: "v0.1.0",
 		Err: nil,
 	}
